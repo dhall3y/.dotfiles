aed912e190795aa4962695058975acbf82543541
dhall3y <dhalley@disroot.org>
2025-10-20 08:07:19 +0200

    update lsp config

--- stow/nvim/dot-config/nvim/lua/custom/plugins/lspconfig.lua
+++ stow/nvim/dot-config/nvim/lua/custom/plugins/lspconfig.lua
@@ -171,6 +171,8 @@ return {
 				clangd = vim.fn.expand("$HOME/.espressif/tools/esp-clang/16.0.1-fe4f10a809/esp-clang/bin/clangd")
 			end
 
+			local capabilities = require("blink.cmp").get_lsp_capabilities()
+
 			---@type MasonLspconfigSettings
 			---@diagnostic disable-next-line: missing-fields
 			-- Enable the following language servers
@@ -268,11 +270,11 @@ return {
 				},
 			}
 
-			---@type MasonLspconfigSettings
-			---@diagnostic disable-next-line: missing-fields
-			require("mason-lspconfig").setup({
-				automatic_enable = vim.tbl_keys(servers or {}),
-			})
+			-----@type MasonLspconfigSettings
+			-----@diagnostic disable-next-line: missing-fields
+			--require("mason-lspconfig").setup({
+			--	automatic_enable = vim.tbl_keys(servers or {}),
+			--})
 
 			local ensure_installed = vim.tbl_keys(servers or {})
 			vim.list_extend(ensure_installed, {
@@ -280,9 +282,23 @@ return {
 			})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
-			for server_name, config in pairs(servers) do
-				vim.lsp.config(server_name, config)
-			end
+			--for server_name, config in pairs(servers) do
+			--	vim.lsp.config(server_name, config)
+			--end
+			require("mason-lspconfig").setup({
+				ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
+				automatic_installation = false,
+				handlers = {
+					function(server_name)
+						local server = servers[server_name] or {}
+						-- This handles overriding only values explicitly passed
+						-- by the server configuration above. Useful when disabling
+						-- certain features of an LSP (for example, turning off formatting for ts_ls)
+						server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+						require("lspconfig")[server_name].setup(server)
+					end,
+				},
+			})
 		end,
 	},
 }
